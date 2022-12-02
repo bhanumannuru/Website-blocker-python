717aeab05e17416a68e7a42216d0570c46d81eb8
bhanumannuru <101179779+bhanumannuru@users.noreply.github.com>
2022-12-02 14:41:08 +0530

    2

--- app.py
+++ app.py
@@ -39,6 +39,17 @@ def block_websites(start_hour, end_hour):
                 hostfile.truncate()
             print("Good Time")
         time.sleep(3)
+        
+        else:
+            with open(default_hoster, "r+") as hostfile:
+                hosts = hostfile.readlines()
+                hostfile.seek(0)
+                for host in hosts:
+                    if not any(site in host for site in sites_to_block):
+                        hostfile.write(host)
+                hostfile.truncate()
+            print("Good Time")
+        time.sleep(3)
 
 
 if __name__ == "__main__":
